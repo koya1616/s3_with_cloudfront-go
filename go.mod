c2cf43b1f11dd744dd9f71fe048425fb2af43c57
koya1616 <aoyamakoya@team-lab.com>
2023-05-18 22:32:10 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module s3_with_cloudfront-go
+
+go 1.18
+
+require (
+  github.com/aws/aws-cdk-go/awscdk/v2 v2.77.0
+  github.com/aws/constructs-go/constructs/v10 v10.0.5
+  github.com/aws/jsii-runtime-go v1.29.0
+)
---
+++ s3_with_cloudfront-go_test.go
@@ -0,0 +1,26 @@
+package main
+
+// import (
+// 	"testing"
+
+// 	"github.com/aws/aws-cdk-go/awscdk/v2"
+// 	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
+// 	"github.com/aws/jsii-runtime-go"
+// )
+
+// example tests. To run these tests, uncomment this file along with the
+// example resource in s3_with_cloudfront-go_test.go
+// func TestS3WithCloudfrontGoStack(t *testing.T) {
+// 	// GIVEN
+// 	app := awscdk.NewApp(nil)
+
+// 	// WHEN
+// 	stack := NewS3WithCloudfrontGoStack(app, "MyStack", nil)
+
+// 	// THEN
+// 	template := assertions.Template_FromStack(stack)
+
+// 	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
+// 		"VisibilityTimeout": 300,
+// 	})
+// }
